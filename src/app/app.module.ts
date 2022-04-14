924d47e2418abee97944e3afad3de9c79dd7f4b3
Trigo00 <trigotex@gmail.com>
2022-04-14 01:41:31 -0300

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
-
+import { contadorComponent } from './contadores/contador/contador.component';
+import { HeroesModule } from './Heroes/Heroes.module';
+import { contadorModule } from './contadores/contador/contador.module';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HeroesModule,
+    contadorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/contadores/contador/contador.module.ts
@@ -0,0 +1,12 @@
+import { NgModule, Component } from '@angular/core';
+import { contadorComponent } from './contador.component';
+
+@NgModule ({
+    declarations: [
+        contadorComponent
+    ],
+    exports: [
+        contadorComponent
+    ],
+})
+export class contadorModule {}
